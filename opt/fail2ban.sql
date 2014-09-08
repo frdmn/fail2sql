8b35dcb6ab42eeb5703d59cd216e5112c42f3c9e
Jonas Friedmann <j@frd.mn>
2014-09-09 00:20:51 +0200

    Improve SQL strucure

--- opt/fail2ban.sql
+++ opt/fail2ban.sql
@@ -4,15 +4,16 @@
 
 DROP TABLE IF EXISTS `fail2ban`;
 CREATE TABLE `fail2ban` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  `protocol` varchar(4) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `protocol` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
   `port` int(11) NOT NULL,
-  `ip` varchar(20) NOT NULL,
-  `count` int(11) NOT NULL,
-  `longitude` varchar(20) DEFAULT NULL,
-  `latitude` varchar(20) DEFAULT NULL,
-  `country` varchar(5) DEFAULT NULL,
-  `geo` varchar(255) DEFAULT NULL,
+  `ip` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `longitude` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `latitude` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `country` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `geo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timestamp` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
